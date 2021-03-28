9562f430a62918501054bbf1544cc92694a363a3
pheaton <paddyheaton264@gmail.com>
2021-03-28 12:38:10 +1100

    added enemy spawning and moving, removed temporary turret behaviour

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 1f;
+
+    Vector3 currentDirection = Vector3.back;
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();    
+    }
+
+    private void Update()
+    {
+        rb.velocity = currentDirection * moveSpeed;
+    }
+}
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+
+    [SerializeField] GameObject enemyPrefab;
+
+    [SerializeField, Range(1, 10)] int spawnDelay;
+    [SerializeField, Range(1, 10)] int maxNumInLane;
+
+    private float timeRemaining = 0f;
+
+    private List<GameObject> spawnedEnemies = new List<GameObject>();
+
+    private void Update()
+    {
+        foreach (GameObject enemy in spawnedEnemies)
+            if (enemy == null)
+                spawnedEnemies.Remove(enemy);
+
+        if (timeRemaining <= 0f)
+            SpawnEnemy();
+
+        else
+            timeRemaining -= Time.deltaTime;
+    }
+
+    private void SpawnEnemy()
+    {
+        timeRemaining = spawnDelay;
+
+        if (spawnedEnemies.Count < maxNumInLane)
+            spawnedEnemies.Add(Instantiate(enemyPrefab, transform.position, Quaternion.identity));
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawCube(transform.position, Vector3.one * 0.5f);
+    }
+
+}
