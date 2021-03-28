2e81e612213fd5a0c527b9ad460aa83af7e35cf1
pheaton <paddyheaton264@gmail.com>
2021-03-28 11:59:56 +1100

    first commit, set up scene and prefabs

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody rb;
+    float movementSpeed = 5f;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+
+        rb.velocity = input * movementSpeed;
+    }
+}
---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+
+    [SerializeField]int maxAmmo = 20;
+    int currentAmmo;
+
+    [SerializeField] float rotationSpeed = 0.1f;
+
+    [SerializeField]GameObject gunObject;
+    int direction = 1;
+
+    private void Update()
+    {
+        SearchSurrounds();
+    }
+
+    private void SearchSurrounds()
+    {
+        Vector3 rotation = gunObject.transform.rotation.eulerAngles;
+
+        if (Mathf.RoundToInt(rotation.y) == 90 || Mathf.RoundToInt(rotation.y) == 270)
+            direction *= -1;
+
+        rotation += Vector3.up * rotationSpeed * direction;
+
+        gunObject.transform.rotation = Quaternion.Euler(rotation);
+
+        //Do a raycast out the front of the turret, using a layer mask with the 'Enemy' layer selected
+        //If hit something, then lock on to it and fire at it
+        //Or do a boxcast/spherecast for a wider view angle
+    }
+}
