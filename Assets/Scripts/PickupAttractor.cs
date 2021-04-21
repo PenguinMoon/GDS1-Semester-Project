90216daab04524f7c045dd7b36a2e9b3b01d2021
EchoCordis <50188197+EchoCordis@users.noreply.github.com>
2021-04-22 02:37:50 +1000

    (US#7) Made currency now move towards the player when close enough

---
+++ Assets/Scripts/PickupAttractor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupAttractor : MonoBehaviour
+{
+    bool isMoving = false;
+    GameObject target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isMoving)
+            transform.parent.position = Vector3.MoveTowards(transform.parent.position, target.transform.position, 10f * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+            target = other.gameObject;
+            isMoving = true;
+        }
+    }
+}
