4cfd6638d4a322bd2d4903865d308af4b72d0b3b
EchoCordis <50188197+EchoCordis@users.noreply.github.com>
2021-04-22 15:25:35 +1000

    (US#8) Made currency flash for a short time before disappearing

--- Assets/Scripts/Bolt.cs
+++ Assets/Scripts/Bolt.cs
@@ -4,17 +4,57 @@ using UnityEngine;
 
 public class Bolt : MonoBehaviour
 {
-    float lifeTime = 0f;
+    //float lifeTime = 0f;
     float maxLifeTime = 20f;
+
+    // How long until the material colour starts flashing e.g. starts flashing after 15secs
+    // NOTE: Time should always be LOWER than maxLifeTime, or else it doesn't flash at all
+    float startFlashTime = 17f;
+
+    Material _material; // The material on the gameobject
+    Color origColour;   // Material's original colour
+
+    private void Start()
+    {
+        _material = GetComponent<Renderer>().material;
+        origColour = _material.color;
+
+        StartCoroutine(DisappearFlash());
+        Destroy(gameObject, maxLifeTime);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if (lifeTime >= maxLifeTime)
-            Destroy(gameObject);
-        else
-            lifeTime += Time.deltaTime;
+        //if (lifeTime >= maxLifeTime)
+        //    Destroy(gameObject);
+        //else
+        //    lifeTime += Time.deltaTime;
 
         transform.Rotate(Vector3.left);
         transform.Rotate(Vector3.up * 0.5f);
+    }
+
+    // Makes the material's colour change between the original colour and white
+    // when the currency is about to disappear
+    IEnumerator DisappearFlash()
+    {
+        yield return new WaitForSeconds(startFlashTime);
+        while (enabled)
+        {
+            _material.color = Color.white;
+            yield return new WaitForSeconds(0.2f);  // Change the float to change delay between flashes
+            _material.color = origColour;
+            yield return new WaitForSeconds(0.2f);  // Change the float to change delay between flashes
+        }
+    }
+
+    private void OnDestroy()
+    {
+        // Destroys the material to prevent too many duplicates of the material.
+        // (When you change the material on a renderer, it creates a duplicate
+        // to not affect the material on other gameobjects. You have to destroy it
+        // to not have a lot of materials that will affect performance.)
+        Destroy(_material);
     }
 }
