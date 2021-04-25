647d5a7f3445e31008a2778d99cad36e5edc43ad
EchoCordis <50188197+EchoCordis@users.noreply.github.com>
2021-04-25 18:01:06 +1000

    (US #11) Made the level select screen scrollable with keyboard/controller
    - Also set certain elements to be the first active item on each canvas when the canvas is activated.
    - Changed some colours of UI elements when they are selected for visibility purposes.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,17 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] GameObject mainCanvas;
     [SerializeField] GameObject lvlSelectCanvas;
 
+    [Header("First Highlighted Button on Each Screen")]
+    [SerializeField] GameObject[] btn;
+
     public void OpenLevelSelect()
     {
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         lvlSelectCanvas.SetActive(true);
         mainCanvas.SetActive(false);
+        EventSystem.current.SetSelectedGameObject(btn[1]);
     }
 
     public void OnExitButtonClicked()
@@ -24,6 +30,7 @@ public class MainMenu : MonoBehaviour
     {
         lvlSelectCanvas.SetActive(false);
         mainCanvas.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(btn[0]);
     }
 
 }
