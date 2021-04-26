b6173c784c1fafccf53f22437b97c4cf7636c300
Rhys Vincent <Rhys.gregory.vincent@gmail.com>
2021-04-26 10:38:56 +1000

    As an enemy, I want to follow a path

---
+++ Assets/EnemyAI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : StateMachine
+{
+    [SerializeField]
+    private NavMeshAgent agent;
+
+    [SerializeField]
+    private Transform goal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent.destination = goal.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/State.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+
+public abstract class State
+{
+    protected EnemyAI _enemyAI;
+
+    public virtual IEnumerator Start()
+    {
+        yield break;
+    }
+
+    public virtual IEnumerator Idle()
+    {
+        yield break;
+    }
+
+    public virtual IEnumerator MoveToGoal()
+    {
+        yield break;
+    }
+
+    public virtual IEnumerator Search()
+    {
+        yield break;
+    }
+
+    public virtual IEnumerator Chase()
+    {
+        yield break;
+    }
+
+    public virtual IEnumerator Attack()
+    {
+        yield break;
+    }
+
+}
