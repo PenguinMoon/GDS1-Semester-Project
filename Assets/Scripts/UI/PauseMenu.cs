e0b2a524ae490158a7f68cc07f5a75d923b68b3e
EchoCordis <50188197+EchoCordis@users.noreply.github.com>
2021-04-27 21:38:40 +1000

    [#12] Fixed pause menu buttons not highlighting correctly

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -1,15 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class PauseMenu : MonoBehaviour
 {
-    [SerializeField] GameObject btn;
+    [SerializeField] Button btn;
 
     private void OnEnable()
     {
-        EventSystem.current.SetSelectedGameObject(btn);
+        EventSystem.current.SetSelectedGameObject(btn.gameObject);
+        btn.OnSelect(null);
     }
 
     private void OnDisable()
