ba4167be21ed9247fe49b598867199711a6251ce
pheaton264 <paddyheaton264@gmail.com>
2021-05-01 15:52:13 +1000

    [#34] Added objective for cannon, a toy castle

--- Assets/Scripts/Objectives/Factory_Oven.cs
+++ Assets/Scripts/Objectives/Factory_Oven.cs
@@ -4,21 +4,11 @@ using UnityEngine;
 
 public class Factory_Oven : MonoBehaviour
 {
-    [SerializeField] Transform doorPivotPoint;
+    [SerializeField] GameObject doorPivotPoint;
+    [SerializeField] float openXRotation = -95;
 
     public void OpenDoor()
     {
-        StartCoroutine(IncrementOpenDoor());
-    }
-
-    IEnumerator IncrementOpenDoor()
-    {
-        while (doorPivotPoint.localRotation.x > -95)
-        {
-            Vector3 rot = doorPivotPoint.localRotation.eulerAngles;
-            rot.x -= 1;
-            doorPivotPoint.localRotation = Quaternion.Euler(rot);
-            yield return new WaitForSeconds(0.01f);
-        }
+        LeanTween.rotate(doorPivotPoint, new Vector3(openXRotation, 0, 0), 2f);
     }
 }
