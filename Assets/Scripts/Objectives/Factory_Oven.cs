de1c2609bb80c164b4f7cbe19628016f89389913
pheaton264 <paddyheaton264@gmail.com>
2021-05-01 14:19:41 +1000

    [#34] created the objective for flamethrowers, a toy oven

---
+++ Assets/Scripts/Objectives/Factory_Oven.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Factory_Oven : MonoBehaviour
+{
+    [SerializeField] Transform doorPivotPoint;
+
+    public void OpenDoor()
+    {
+        StartCoroutine(IncrementOpenDoor());
+    }
+
+    IEnumerator IncrementOpenDoor()
+    {
+        while (doorPivotPoint.localRotation.x > -95)
+        {
+            Vector3 rot = doorPivotPoint.localRotation.eulerAngles;
+            rot.x -= 1;
+            doorPivotPoint.localRotation = Quaternion.Euler(rot);
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+}
---
+++ Assets/Scripts/Objectives/KioskObjective.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class KioskObjective : MonoBehaviour
+{
+    [SerializeField] Text currencyAmountTxt;
+    [SerializeField] int requiredBits = 0;
+    [SerializeField] UnityEvent onObjectiveRepaired;
+
+    int remainingBits = 0;
+
+    private void Start()
+    {
+        remainingBits = requiredBits;
+
+        //Assign amount to UI Element
+        currencyAmountTxt.text = requiredBits.ToString();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Entered " + other);
+
+        ReceiveBits(1);
+    }
+
+    private void ReceiveBits(int amount)
+    {
+        if (remainingBits > 0)
+        {
+            remainingBits -= amount;
+
+            currencyAmountTxt.text = remainingBits.ToString();
+
+            if (remainingBits <= 0)
+                OnObjectiveFunded();
+        }
+    }
+
+    private void OnObjectiveFunded()
+    {
+        onObjectiveRepaired.Invoke();
+        Destroy(gameObject);
+    }
+}
