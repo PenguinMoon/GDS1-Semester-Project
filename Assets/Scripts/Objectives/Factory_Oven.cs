9c1b44f22d99c579229de07570b66af01d4b554b
pheaton <paddyheaton264@gmail.com>
2021-05-02 20:49:47 +1000

    [#34] Disabled objective door collisions whilst moving, so player doesnt clip out of map

--- Assets/Scripts/Objectives/Factory_Oven.cs
+++ Assets/Scripts/Objectives/Factory_Oven.cs
@@ -7,8 +7,24 @@ public class Factory_Oven : MonoBehaviour
     [SerializeField] GameObject doorPivotPoint;
     [SerializeField] float openXRotation = -95;
 
+    Collider[] doorChildren;
+
+    private void Awake()
+    {
+        doorChildren = GetComponentsInChildren<Collider>();
+    }
+
     public void OpenDoor()
     {
-        LeanTween.rotateLocal(doorPivotPoint, new Vector3(openXRotation, 0, 0), 2f);
+        SetChildrenCollision(false);
+        LeanTween.rotateLocal(doorPivotPoint, new Vector3(openXRotation, 0, 0), 2f).setOnComplete(() => SetChildrenCollision(true));
+    }
+
+    private void SetChildrenCollision(bool isEnabled)
+    {
+        foreach (Collider col in doorChildren)
+        {
+            col.enabled = isEnabled;
+        }
     }
 }
