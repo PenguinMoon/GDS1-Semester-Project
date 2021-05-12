5483d32a318863b5e83df14ea92996c7ba6f070e
pheaton <paddyheaton264@gmail.com>
2021-05-12 20:26:46 +1000

    [#35] added a bridge objective

---
+++ Assets/Scripts/Objectives/Objective_Bridge.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objective_Bridge : MonoBehaviour
+{
+    [SerializeField] Transform[] pivotPoints;
+
+    void Start() 
+    {
+        SetHalfRotationTo(90, 0.1f);
+    }
+
+    public void OnRepair() {
+        SetHalfRotationTo(0, 2f);
+    }
+
+    void SetHalfRotationTo(float rot, float time) {
+        foreach (Transform point in pivotPoints) {
+            LeanTween.rotateX(point.gameObject, rot, time);
+        }
+    }
+}
