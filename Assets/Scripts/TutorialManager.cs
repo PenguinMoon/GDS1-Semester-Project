8db779f62df2ff1f564af9a76f00e04afd76b015
tuonglong <tuonglong200034@gmail.com>
2021-05-28 22:16:10 +1000

    [#25] Fixed tutorial bugs due to multiplayer implementation

--- Assets/Scripts/TutorialManager.cs
+++ Assets/Scripts/TutorialManager.cs
@@ -26,6 +26,7 @@ public class TutorialManager : MonoBehaviour
     {
         multiplayerManager = FindObjectOfType<MultiplayerManager>();
         player = FindObjectOfType<Player>();
+        turret = FindObjectOfType<SmartTurret>();
         dialogueManager = FindObjectOfType<DialogueManager>();
     }
 
@@ -35,54 +36,60 @@ public class TutorialManager : MonoBehaviour
         switch (stepIndex)
         {
             case 0:
-            arrow.transform.position = turretPlate.transform.position;
-            break;
+                arrow.transform.position = turretPlate.transform.position;
+                break;
 
             case 1:
-            arrow.transform.position = turretPlate.transform.position;
-            break;
+                arrow.transform.position = turretPlate.transform.position;
+                break;
 
             case 4:
-            arrow.transform.position = workbench.transform.position;
-            break;
+                arrow.transform.position = workbench.transform.position;
+                break;
 
             case 5:
-            arrow.transform.position = turretPlate2.transform.position;
-            break;
+                arrow.transform.position = turretPlate2.transform.position;
+                break;
 
             case 6:
-            arrow.transform.position = turret.transform.position;
-            break;
+                arrow.transform.position = turret.transform.position;
+                break;
 
             case 7:
-            arrow.transform.position = workbench2.transform.position;
-            break;
+                arrow.transform.position = workbench2.transform.position;
+                break;
 
             case 8:
-            arrow.transform.position = turret.transform.position;
-            break;
+                arrow.transform.position = turret.transform.position;
+                break;
 
             default:
-                arrow.transform.position = new Vector3(1000,1000,1000);
-            break;
+                arrow.transform.position = new Vector3(1000, 1000, 1000);
+                break;
         }
 
-        if (player == null) {
+        if (player == null)
+        {
             player = FindObjectOfType<Player>();
             turret = FindObjectOfType<SmartTurret>();
-
-
-            if (player) {
-                dialogueManager.DisplayNextSentence();
-                StartCoroutine(AutoDialogue(5f));
-            }
-            return;
         }
 
-        if (stepIndex == 0 && player.selectedObject.tag == "Turret") // Player picks up initial turret
+        /*        if (player == null) {
+                    player = FindObjectOfType<Player>();
+                    turret = FindObjectOfType<SmartTurret>();
+
+
+                    if (player) {
+                        dialogueManager.DisplayNextSentence();
+                        StartCoroutine(AutoDialogue(5f));
+                    }
+                    return;
+                }*/
+
+        if (stepIndex == 0) // Player picks up initial turret
         {
             stepIndex++;
-            dialogueManager.DisplayNextSentence();
+            StartCoroutine("AutoDialogue", 2f);
         }
 
         if (stepIndex == 1 && turretPlate.placedObject && turretPlate.placedObject.tag == "Turret") // Player places turret down on correct plate
@@ -97,19 +104,19 @@ public class TutorialManager : MonoBehaviour
             StartCoroutine("AutoDialogue", 3f);
         }
 
-        if (stepIndex == 3 && multiplayerManager.CanAffordItem(3, 0)) // Wait for the player to pick up at least 3 currency
+        if (stepIndex == 3 && multiplayerManager.inventory["Bits"] >= 3) // Wait for the player to pick up at least 3 currency
         {
             stepIndex++;
             dialogueManager.DisplayNextSentence();
         }
 
-        if (stepIndex == 4 && player.selectedObject.tag == "Turret") // Wait for the player to craft new turret
+        if (stepIndex == 4 && player.selectedObject && player.selectedObject.tag == "Turret") // Wait for the player to craft new turret
         {
             stepIndex++;
             dialogueManager.DisplayNextSentence();
         }
 
-        if (stepIndex == 5 && turretPlate2.placedObject.tag == "Turret") // Wait for the player to place new turret down
+        if (stepIndex == 5 && turretPlate2.placedObject && turretPlate2.placedObject.tag == "Turret") // Wait for the player to place new turret down
         {
             stepIndex++;
             dialogueManager.DisplayNextSentence();
@@ -118,10 +125,10 @@ public class TutorialManager : MonoBehaviour
         if (stepIndex == 6) // Wait for a few seconds while the player whacks the turret
         {
             stepIndex++;
-            StartCoroutine("AutoDialogue", 8f);
+            StartCoroutine("AutoDialogue", 1f);
         }
 
-        if (stepIndex == 7 && player.selectedObject.tag == "Object") // Wait player to buy ammo
+        if (stepIndex == 7 && player.selectedObject && player.selectedObject.tag == "Object") // Wait player to buy ammo
         {
             stepIndex++;
             dialogueManager.DisplayNextSentence();
