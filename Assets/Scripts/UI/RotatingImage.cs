9ad34d9b513eb2cd0dfe826004dad5dd058904fc
EchoCordis <50188197+EchoCordis@users.noreply.github.com>
2021-06-20 18:59:44 +1000

    Added loading icon to loading screen

---
+++ Assets/Scripts/UI/RotatingImage.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingImage : MonoBehaviour
+{
+    [SerializeField] float rotSpeed = 100f;
+    [SerializeField] Vector3 dir = Vector3.back;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(dir * Time.deltaTime * rotSpeed);
+    }
+}
